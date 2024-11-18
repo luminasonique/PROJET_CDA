121da7a0f893be36030e96f21a93a78c7ed4451e
luminasonique <luminasonique@gmail.com>
2024-11-18 18:30:55 +0100

    first commit

---
+++ src/Entity/User.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\UserRepository;
+use App\Traits\DateTraits;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ApiResource]
+class User
+{
+    use DateTraits; // Applying the trait
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $first_name = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $last_name = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $password = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?array $roles = [];
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): static
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(string $last_name): static
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRoles(): ?array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(?array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
---
+++ src/Entity/UserInfo.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\UserInfoRepository;
+use App\Traits\DateTraits;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserInfoRepository::class)]
+#[ApiResource]
+class UserInfo
+{
+    use DateTraits; // Applying the trait
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $user_cv = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $user_linkedin = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $user_git = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $user_description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserCv(): ?string
+    {
+        return $this->user_cv;
+    }
+
+    public function setUserCv(?string $user_cv): static
+    {
+        $this->user_cv = $user_cv;
+
+        return $this;
+    }
+
+    public function getUserLinkedin(): ?string
+    {
+        return $this->user_linkedin;
+    }
+
+    public function setUserLinkedin(?string $user_linkedin): static
+    {
+        $this->user_linkedin = $user_linkedin;
+
+        return $this;
+    }
+
+    public function getUserGit(): ?string
+    {
+        return $this->user_git;
+    }
+
+    public function setUserGit(?string $user_git): static
+    {
+        $this->user_git = $user_git;
+
+        return $this;
+    }
+
+    public function getUserDescription(): ?string
+    {
+        return $this->user_description;
+    }
+
+    public function setUserDescription(?string $user_description): static
+    {
+        $this->user_description = $user_description;
+
+        return $this;
+    }
+}
